e69d00e3926cc5ea46dab8a3e9b492a734110097
profxj <xavier@ucolick.org>
2025-09-25 07:17:39 -0700

    grab images

--- fronts/dbof/utils.py
+++ fronts/dbof/utils.py
@@ -1,22 +1,71 @@
 """ Utility functions for interfacing with DBOF """
 
+import os
 import numpy as np
+import h5py
 
+from fronts import io as fronts_io
 from fronts.dbof import io as dbof_io
 
-def find_entry(dbof_json_dict:(str|dict), sdict:dict):
+from IPython import embed
+
+def find_entry(dbof_json_dict:(str|dict), sdict:dict, debug:bool=False):
+    """
+    Find an entry in a DBOF (Database of Oceanographic Fronts) table that matches the specified criteria.
+    Parameters:
+    -----------
+    dbof_json_dict : str | dict
+        The DBOF table, either as a JSON file path (str) or a dictionary (dict).
+    sdict : dict
+        A dictionary containing the search criteria. Keys should correspond to column names in the DBOF table,
+        and values represent the values to match.
+    debug : bool, optional
+        If True, prints debug information about the matching process. Default is False.
+    Returns:
+    --------
+    int
+        The index of the matching entry in the DBOF table.
+        Returns -1 if no match is found.
+    Raises:
+    -------
+    KeyError
+        If a key in `sdict` is not found in the DBOF table.
+    ValueError
+        If multiple matches are found for the given criteria.
+    Notes:
+    ------
+    - For the 'datetime' key in `sdict`, the value is converted to `numpy.datetime64` if it is not already in that format.
+    - The function uses logical AND to combine matches across multiple keys in `sdict`.
+    Debugging:
+    ----------
+    When `debug` is True, the function prints:
+    - The key being matched and the value being searched for.
+    - The number of matches found for the current key.
+    - The total number of matches after combining with previous keys.
+    """
 
     # Load up main table
     dbof_table = dbof_io.load_main_table(dbof_json_dict)
 
     # Now match on sdict
-    match = np.zeros(len(dbof_table), dtype=bool)
+    match = np.ones(len(dbof_table), dtype=bool)
 
     for key in sdict.keys():
-        if key in dbof_table.keys():
-            match = match & (dbof_table[key].values == sdict[key])
-        else:
+        if key not in dbof_table.keys():
             raise KeyError("Key {} not in table".format(key))
+        # Match
+        if key == 'datetime':
+            # Convert to numpy datetime64
+            if not isinstance(sdict[key], np.datetime64):
+                sdict[key] = np.datetime64(sdict[key])
+        match = match & (dbof_table[key].values == sdict[key])
+        #
+        if debug:
+            print(f'Key: {key}, looking for {sdict[key]}')
+            print(f'  Found {np.sum(dbof_table[key].values == sdict[key])} matches')
+            print(f'  Total matches now {np.sum(match)}')
+            embed(header="in dbof.utils.find_entry 31")
+            
 
     if np.sum(match) == 0:
         print("No match found. Returning -1")
@@ -26,3 +75,48 @@ def find_entry(dbof_json_dict:(str|dict), sdict:dict):
     else:
         return dbof_table[match].index.values[0]
     
+def grab_fields(dbof_json_dict:(str|dict), tidx:int, fields:(list|str)):
+
+    # Load the json dict 
+    dbof_dict = fronts_io.loadjson(dbof_json_dict)
+
+    # Load up main table
+    dbof_table = dbof_io.load_main_table(dbof_dict)
+    UID = dbof_table.UID[tidx]
+    print(f"Grabbing fields for UID: {UID}")
+
+    # Fields
+    if isinstance(fields, str) and fields == 'all':
+        fields = dbof_dict['fields']
+
+    # Loop on fields
+    field_data = {}
+    for field in fields:
+        # Load up the field meta if not there
+        meta_tbl = dbof_io.load_meta_table(dbof_dict, field)
+        if meta_tbl is None:
+            print(f"Field {field} not found")
+            continue
+
+        # Match on UID
+        mt = np.where(UID == meta_tbl.UID.values)[0]
+        if len(mt) == 0:
+            print(f"Field {field} not found for UID {UID}")
+            continue
+
+        # Field file
+        field_file = dbof_io.field_path(field, dbof_dict) 
+        if not os.path.exists(field_file):
+            print(f"Field file {field_file} not found")
+            continue 
+
+        # Do it
+        fc = h5py.File(field_file, 'r')
+        group = str(meta_tbl.group.values[mt[0]])
+        gidx = int(meta_tbl.gidx.values[mt[0]])
+        cutout = fc[group][gidx]
+        fc.close()
+        field_data[field] = cutout
+
+    # Return
+    return field_data
---
+++ fronts/tests/test_dbof_utils.py
@@ -0,0 +1,37 @@
+
+import os
+from importlib.resources import files
+
+from fronts.dbof import utils as dbof_utils
+from fronts.dbof import io as dbof_io
+
+from IPython import embed
+
+dbof_dev_file = os.path.join(str(files('fronts')._paths[0]), 'runs', 'dbof', 'dev',
+                        'llc4320_dbof_dev.json')
+
+def test_find_entry():
+    """
+    Test the find_entry function from dbof_utils
+    """
+    # Grab JSON file
+    #embed(header="14 @dbof_utils.find_entry")                        
+
+    # Load table
+    dbof_tbl = dbof_io.load_main_table(dbof_dev_file)
+
+    # Test case
+    lat = 56.752953
+    lon = -170.447922
+    datetime = '2011-09-30'
+
+    idx = dbof_utils.find_entry(dbof_dev_file, 
+                                {'lat':lat, 'lon':lon, 'datetime':datetime},
+                                debug=False)
+
+    assert idx == 0
+    #print("Found index: {}".format(idx))
+    #embed(header="@dbof_utils.find_entry")                        
+
+tidx = 0
+field_data = dbof_utils.grab_fields(dbof_dev_file, tidx, 'all')
