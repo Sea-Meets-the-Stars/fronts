2b28a3afd3ad1e47d92cb7f944cc23b0d8f36e14
J. Xavier Prochaska <xavier@ucolick.org>
2026-03-26 22:08:38 -0700

    llc

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ if os.path.exists('README.md'):
 setup_keywords['provides'] = [setup_keywords['name']]
 setup_keywords['requires'] = ['Python (>=3.11.0)']
 setup_keywords['install_requires'] = [
+    #'dbof-in-native-grid @ git+https://github.com/Sea-Meets-the-Stars/llc4320-native-grid-preprocessing.git@properties',
     'torch',
     'torchvision', 'seaborn', 'smart-open[s3]', 'pyarrow',
     'scikit-learn', 'scikit-image', 'tqdm', 'astropy', 'astropy-healpix',
