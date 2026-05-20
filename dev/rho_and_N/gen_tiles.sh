8768375b33ef45178195e761b87fc528f7203f41
J. Xavier Prochaska <xavier@ucolick.org>
2026-05-19 17:57:40 -0700

    oops

--- dev/rho_and_N/gen_tiles.sh
+++ dev/rho_and_N/gen_tiles.sh
@@ -10,18 +10,20 @@ GEN_FIGS=false
 if $RUN_TP; then
     # Density
     python dev/tiles/generate_tile.py --i 9800 \
-    --j 9000 --timestamp '2012-11-09 12:00:00'
-    --output $OS_OGCM/LLC/Fronts/V3/20121109_120000/tiles
+        --j 9000 --timestamp '2012-11-09 12:00:00' \
+        --output $OS_OGCM/LLC/Fronts/V3/20121109_120000/tiles
+fi
 
 ### Figures
 if $RUN_TP && $GEN_FIGS; then
     python /home/xavier/Oceanography/python/fronts/dev/rho_and_N/plot_top_N_density_profiles.py \
-    --density-tile     $OS_OGCM/LLC/Fronts/V3/20121109_120000/density_tile301_20121109T12.nc \
-    --gradb2           $OS_OGCM/LLC/Fronts/V3/20121109_120000/LLC4320_2012-11-09T12_00_00_gradb2_v3.nc \
-    --labels           $OS_OGCM/LLC/Fronts/V3/20121109_120000/labeled_fronts_global_20121109T12_00_00_v3_bin_D.npy \
-    --front-index      $OS_OGCM/LLC/Fronts/V3/20121109_120000/front_index_20121109T12_00_00_v3_bin_D.parquet \
-    --front-properties $OS_OGCM/LLC/Fronts/V3/20121109_120000/front_properties_20121109T12_00_00_v3_bin_D.parquet \
-    --N 10 \
-    --i-rect-range 9600 9950 \
-    --j-rect-range 8950 9200 \
-    --outdir .
+        --density-tile     $OS_OGCM/LLC/Fronts/V3/20121109_120000/density_tile301_20121109T12.nc \
+        --gradb2           $OS_OGCM/LLC/Fronts/V3/20121109_120000/LLC4320_2012-11-09T12_00_00_gradb2_v3.nc \
+        --labels           $OS_OGCM/LLC/Fronts/V3/20121109_120000/labeled_fronts_global_20121109T12_00_00_v3_bin_D.npy \
+        --front-index      $OS_OGCM/LLC/Fronts/V3/20121109_120000/front_index_20121109T12_00_00_v3_bin_D.parquet \
+        --front-properties $OS_OGCM/LLC/Fronts/V3/20121109_120000/front_properties_20121109T12_00_00_v3_bin_D.parquet \
+        --N 10 \
+        --i-rect-range 9600 9950 \
+        --j-rect-range 8950 9200 \
+        --outdir .
+fi
