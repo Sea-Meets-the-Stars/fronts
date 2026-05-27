2660e20d15fcba98cacfe1079c27be224c87ae2b
J. Xavier Prochaska <xavier@ucolick.org>
2026-05-27 09:17:41 -0700

    scripting

---
+++ dev/mld/gen_tiles.py
@@ -0,0 +1,45 @@
+# Code used to generate the tiles for the MLD investigation
+
+import sys
+import os
+
+# locals
+from mld_defs import MLD_DEFS
+
+from dbof.tiles import tile_utils
+
+def main(flg):
+    flg = int(flg)
+
+    # Generate tiles
+    if flg == 1:
+        timestamp = '2012-11-09 12:00:00'
+        output = os.path.join(os.getenv('OS_OGCM'), 'LLC',
+            'Fronts', 'V4', '20121109_120000', 'tiles')
+
+        properties = ['density', 'temperature']
+        regions = ['tropical_pacific']
+
+        # Loop me
+        for region in regions:
+            for prop in properties:
+                tile_utils.run(
+                    i_rect=MLD_DEFS[region]['i_j'][0],
+                    j_rect=MLD_DEFS[region]['i_j'][1],
+                    timestamp=timestamp,
+                    property=prop,
+                    output=output)
+
+    # Fit the lowest-bbp synthetic PACE spectrum with BING
+    if flg == 2:
+        fit_lowest_bbp()
+
+
+# Command line
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        flg = 1
+    else:
+        flg = sys.argv[1]
+
+    main(flg)
---
+++ dev/mld/mld_defs.py
@@ -0,0 +1,15 @@
+# Define locations and the like for MLD investigation
+
+
+MLD_DEFS = {}
+
+# Tropical Pacific
+tropical_pacific = {
+    'name': 'tropical_pacific',
+    'i_j': (9800, 9000),
+    'i_range': (9600, 9950),
+    'j_range': (8950, 9200),
+    'tile': 301
+}
+
+MLD_DEFS['tropical_pacific'] = tropical_pacific
